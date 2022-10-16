67b8473f35a04576c8aab455b2f297b5a67728e9
Steven Kinnunen <71653031+StevenKinnunen@users.noreply.github.com>
2022-10-16 15:05:11 -0400

    Updated Titles, labels and comments
    Added Titles for each plot, uniform colour scheme for plots, axis labels, and legend titles. Added comments to clarify the components of the plot.

--- German Credit Data.R
+++ German Credit Data.R
@@ -7,25 +7,36 @@ library("ROSE") # This library allows us to use ovun.sample() to balance the cla
 
 # Dataset retrieved from: https://www.openml.org/search?type=data&status=active&id=31
 
-#setwd("~/Humber/Fall 2022/Big Data 2") # Location of the file
+setwd("~/Humber/Fall 2022/Big Data 2") # Location of the file
 
 credit_df <- read.arff("dataset_31_credit-g.arff") #reading an arff file as a dataframe
 
+class_palette <- c(good = "#000066", bad = "#660000") # setting consistent color palette for class values,
+# good are blue, whereas bad are red
+
 
 # This plot allows us to look at the count of 'good' and 'bad' values in class column
-class_plot <- ggplot(credit_df, aes(x=class, fill=class)) + geom_bar()
+class_plot <- ggplot(credit_df, aes(x=class, fill=class)) + geom_bar() +
+  scale_fill_manual(values = class_palette) + # Sets the colors to class_palette and fill by manually
+  ggtitle("Count of Customers with Good and Bad Credit") + # Sets a title for the plot
+  labs(y="Count", x="Credit Type") + # Sets the y and x axis labels respectively for the plot
+  theme(plot.title = element_text(hjust=0.5), legend.position = "none") # Centre the title and
+# suppresses the legend since it is redundant in this plot
 class_plot 
 
 
 # Creates a new dataframe that will allow us to reorder based on count of purpose column
 purpose_df <- credit_df %>%
-  group_by(purpose) %>%
-  mutate(purpose_occur=n()) 
+  group_by(purpose) %>% # Selecting the purpose column to group by
+  mutate(purpose_occur=n()) # adds column purpose_occur, which counts how often the specific value of purpose 
+# occurs and adds the total count to each row with grouped purpose value
 
 # Creates a plot based on the purpose dataframe, orders by descending values for count
 purpose_plot <- ggplot(purpose_df, aes(x=reorder(purpose, -purpose_occur), fill=class)) + 
-  geom_bar(stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
-  facet_wrap(.~class) # facet wrap produces multiple plots (2) based on class values
+  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) + #
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Count of Customers by Purpose of Loan and Credit Type") + labs(y="Count", x="Purpose of Loan") +
+  theme(plot.title = element_text(hjust=0.5))
 purpose_plot 
 
 
@@ -36,8 +47,10 @@ job_df <- credit_df %>%
 
 # Creates a plot based on the job dataframe, orders by descending values for count
 job_plot <- ggplot(job_df, aes(x=reorder(job, -job_occur), fill=class)) + 
-  geom_bar(stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
-  facet_wrap(.~class) # facet wrap produces multiple plots (2) based on class values
+  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Count of Customers by Occupation and Credit Type") + labs(y="Count", x="Occupation of Applicant") +
+  theme(plot.title = element_text(hjust=0.5))
 job_plot 
 
 
@@ -48,8 +61,10 @@ history_df <- credit_df %>%
 
 # Creates a plot based on the history dataframe, orders by descending values for count
 history_plot <- ggplot(history_df, aes(x=reorder(credit_history, -history_occur), fill=class)) + 
-  geom_bar(stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
-  facet_wrap(.~class) # facet wrap produces multiple plots (2) based on class values
+  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Count of Customers by Credit History and Credit Type") + labs(y="Count", x="Credit History") +
+  theme(plot.title = element_text(hjust=0.5))
 history_plot 
 
 
@@ -60,25 +75,33 @@ savings_df <- credit_df %>%
 
 # Creates a plot based on the savings dataframe, orders by descending values for count
 savings_plot <- ggplot(savings_df, aes(x=reorder(savings_status, -savings_occur), fill=class)) + 
-  geom_bar(stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
-  facet_wrap(.~class) # facet wrap produces multiple plots (2) based on class values
+  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Count of Customers by Savings and Credit Type") + labs(y="Count", x="Savings") +
+  theme(plot.title = element_text(hjust=0.5))
 savings_plot 
 
 
 # Creates a density plot based on original credit dataframe to show proportion of class values by credit_amount
 # low alpha value allows us to view overlapping densities
-amount_plot <- ggplot(credit_df, aes(x=credit_amount, fill=class)) + geom_density(alpha=0.25)
+amount_plot <- ggplot(credit_df, aes(x=credit_amount, fill=class)) + geom_density(alpha=0.50) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Density of Customers by Loan Amount and Credit Type") + labs(y="Proportion", x="Loan Amount") +
+  theme(plot.title = element_text(hjust=0.5))
 amount_plot
 
 
 # Creates a density plot based on original credit dataframe to show proportion of class values by age
 # low alpha value allows us to view overlapping densities
-age_plot <- ggplot(credit_df, aes(x=age, fill=class)) + geom_density(alpha=0.25)
+age_plot <- ggplot(credit_df, aes(x=age, fill=class)) + geom_density(alpha=0.50) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Density of Customers by Age and Credit Type") + labs(y="Proportion", x="Age") +
+  theme(plot.title = element_text(hjust=0.5))
 age_plot
 
 
-# We create a new dataframe that undersamples from the 'good' records for the class target variable
-credit_df_balanced <- ovun.sample(class~., data=credit_df, method="under", seed=42)$data
+# We create a new dataframe that oversamples from the 'good' records for the class target variable
+credit_df_balanced <- ovun.sample(class~., data=credit_df, method="over", seed=42)$data
 table(credit_df_balanced$class) #table checks the number of each class value in balanced dataframe
 
 
@@ -89,8 +112,10 @@ purpose_df_balanced <- credit_df_balanced %>%
 
 # Creates a plot based on the purpose dataframe, orders by descending values for count
 purpose_plot_bal <- ggplot(purpose_df_balanced, aes(x=reorder(purpose, -purpose_occur), fill=class)) + 
-  geom_bar(stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
-  facet_wrap(.~class) # facet wrap produces multiple plots (2) based on class values
+  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Count of Customers by Purpose of Loan and Credit Type") + labs(y="Count", x="Purpose of Loan") +
+  theme(plot.title = element_text(hjust=0.5))
 purpose_plot_bal
 
 
@@ -101,8 +126,10 @@ job_df_balanced <- credit_df_balanced %>%
 
 # Creates a plot based on the job dataframe, orders by descending values for count
 job_plot_bal <- ggplot(job_df_balanced, aes(x=reorder(job, -job_occur), fill=class)) + 
-  geom_bar(stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
-  facet_wrap(.~class) # facet wrap produces multiple plots (2) based on class values
+  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Count of Customers by Occupation and Credit Type") + labs(y="Count", x="Occupation of Applicant") +
+  theme(plot.title = element_text(hjust=0.5))
 job_plot_bal
 
 
@@ -113,8 +140,10 @@ history_df_balanced <- credit_df_balanced %>%
 
 # Creates a plot based on the history dataframe, orders by descending values for count
 history_plot_balanced <- ggplot(history_df_balanced, aes(x=reorder(credit_history, -history_occur), fill=class)) + 
-  geom_bar(stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
-  facet_wrap(.~class) # facet wrap produces multiple plots (2) based on class values
+  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Count of Customers by Credit History and Credit Type") + labs(y="Count", x="Credit History") +
+  theme(plot.title = element_text(hjust=0.5))
 history_plot_balanced
 
 
@@ -125,18 +154,26 @@ savings_df_balanced <- credit_df_balanced %>%
 
 # Creates a plot based on the savings dataframe, orders by descending values for count
 savings_plot_balanced <- ggplot(savings_df_balanced, aes(x=reorder(savings_status, -savings_occur), fill=class)) + 
-  geom_bar(stat="count") + theme(axis.text.x = element_text(angle = 90)) + 
-  facet_wrap(.~class) # facet wrap produces multiple plots (2) based on class values
+  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Count of Customers by Savings and Credit Type") + labs(y="Count", x="Savings") +
+  theme(plot.title = element_text(hjust=0.5))
 savings_plot_balanced  
 
 
 # Creates a density plot based on original credit dataframe to show proportion of class values by credit_amount
 # low alpha value allows us to view overlapping densities
-amount_plot_balanced <- ggplot(credit_df_balanced, aes(x=credit_amount, fill=class)) + geom_density(alpha=0.25)
+amount_plot_balanced <- ggplot(credit_df_balanced, aes(x=credit_amount, fill=class)) + geom_density(alpha=0.50) +
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Density of Customers by Loan Amount and Credit Type") + labs(y="Proportion", x="Loan Amount") +
+  theme(plot.title = element_text(hjust=0.5))
 amount_plot_balanced
 
 
 # Creates a density plot based on original credit dataframe to show proportion of class values by age
 # low alpha value allows us to view overlapping densities
-age_plot_balanced <- ggplot(credit_df_balanced, aes(x=age, fill=class)) + geom_density(alpha=0.25)
+age_plot_balanced <- ggplot(credit_df_balanced, aes(x=age, fill=class)) + geom_density(alpha=0.50) + 
+  scale_fill_manual(name="Credit Type", values = class_palette) +
+  ggtitle("Density of Customers by Age and Credit Type") + labs(y="Proportion", x="Age") +
+  theme(plot.title = element_text(hjust=0.5))
 age_plot_balanced
