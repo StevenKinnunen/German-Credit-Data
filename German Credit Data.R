d919d0d6dd5799445334e4b57910ac694b89f401
Lorenzo Pepito <76994690+pepsblue@users.noreply.github.com>
2022-10-19 19:00:31 -0400

    Create German Credit Data.R
    Creates a custom function to generate a modified violin plot.  Split violin plot uses savings status, credit amount, and distinguished by credit class. Original code from stackoverflow: https://stackoverflow.com/questions/35717353/split-violin-plot-with-ggplot2.

--- German Credit Data.R
+++ German Credit Data.R
@@ -186,21 +186,42 @@ property_plot
 personvcredit_plot = ggplot(data = credit_df, aes(x= personal_status, colour = class)) + geom_bar(position = "dodge", stat = "count")
 personvcredit_plot
 
-#Boxplot housing type, credit amount_SM
-p2 <- ggplot(credit_df, aes(x=housing, y=credit_amount, fill=class)) + 
-  geom_boxplot() +
-  facet_wrap(~housing, scale="free")
-p2
 
-#Boxplot job type and credit amount_SM
-p1 <- ggplot(credit_df, aes(x=job, y=credit_amount, fill=class)) + 
-  geom_boxplot() +
-  facet_wrap(~class) + coord_flip()
-p1
+# Creates a function for an asymmetrical violin plot integrating a dichotomous variable.
+GeomSplitViolin <- ggproto("GeomSplitViolin", GeomViolin, 
+                           draw_group = function(self, data, ..., draw_quantiles = NULL) {
+                             data <- transform(data, xminv = x - violinwidth * (x - xmin), xmaxv = x + violinwidth * (xmax - x))
+                             grp <- data[1, "group"]
+                             newdata <- plyr::arrange(transform(data, x = if (grp %% 2 == 1) xminv else xmaxv), if (grp %% 2 == 1) y else -y)
+                             newdata <- rbind(newdata[1, ], newdata, newdata[nrow(newdata), ], newdata[1, ])
+                             newdata[c(1, nrow(newdata) - 1, nrow(newdata)), "x"] <- round(newdata[1, "x"])
+                             
+                             if (length(draw_quantiles) > 0 & !scales::zero_range(range(data$y))) {
+                               stopifnot(all(draw_quantiles >= 0), all(draw_quantiles <=
+                                                                         1))
+                               quantiles <- ggplot2:::create_quantile_segment_frame(data, draw_quantiles)
+                               aesthetics <- data[rep(1, nrow(quantiles)), setdiff(names(data), c("x", "y")), drop = FALSE]
+                               aesthetics$alpha <- rep(1, nrow(quantiles))
+                               both <- cbind(quantiles, aesthetics)
+                               quantile_grob <- GeomPath$draw_panel(both, ...)
+                               ggplot2:::ggname("geom_split_violin", grid::grobTree(GeomPolygon$draw_panel(newdata, ...), quantile_grob))
+                             }
+                             else {
+                               ggplot2:::ggname("geom_split_violin", GeomPolygon$draw_panel(newdata, ...))
+                             }
+                           })
 
-#Density Residence since_SM
-residence_since_plot_balanced <- ggplot(credit_df_balanced, aes(x=residence_since, fill=class)) + geom_density(alpha=0.50) + 
-  scale_fill_manual(name="Credit Type", values = class_palette) +
-  ggtitle("Density of Customers by Residence Since") + labs(y="Proportion", x="Residence_Since") +
-  theme(plot.title = element_text(hjust=0.5))
-residence_since_plot_balanced
+geom_split_violin <- function(mapping = NULL, data = NULL, stat = "ydensity", position = "identity", ..., 
+                              draw_quantiles = NULL, trim = TRUE, scale = "area", na.rm = FALSE, 
+                              show.legend = NA, inherit.aes = TRUE) {
+  layer(data = data, mapping = mapping, stat = stat, geom = GeomSplitViolin, 
+        position = position, show.legend = show.legend, inherit.aes = inherit.aes, 
+        params = list(trim = trim, scale = scale, draw_quantiles = draw_quantiles, na.rm = na.rm, ...))
+}
+
+# Create a split violin plot for savings and credit, distinguishing good from bad classes of credit.
+credit_by_savings_plot <- ggplot(savings_df_balanced, aes(x=reorder(savings_status, -savings_occur), 
+                                                          y=credit_amount, fill=class)) +
+  geom_split_violin() + theme_minimal() + 
+  labs(x="Savings Status", y="Credit",title="Credit Density by Savings and Class")
+credit_by_savings_plot
