a910263400c6829540e84b61583291eb5c55137c
Steven Kinnunen <71653031+StevenKinnunen@users.noreply.github.com>
2022-11-01 12:35:51 -0400

    Final updates

--- German Credit Data.R
+++ German Credit Data.R
@@ -25,81 +25,6 @@ class_plot <- ggplot(credit_df, aes(x="", y=class, fill=class)) + geom_bar(stat=
 class_plot 
 
 
-# Creates a new dataframe that will allow us to reorder based on count of purpose column
-purpose_df <- credit_df %>%
-  group_by(purpose) %>% # Selecting the purpose column to group by
-  mutate(purpose_occur=n()) # adds column purpose_occur, which counts how often the specific value of purpose 
-# occurs and adds the total count to each row with grouped purpose value
-
-# Creates a plot based on the purpose dataframe, orders by descending values for count
-purpose_plot <- ggplot(purpose_df, aes(x=reorder(purpose, -purpose_occur), fill=class)) + 
-  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) + #
-  scale_fill_manual(name="Credit Type", values = class_palette) +
-  ggtitle("Count of Customers by Purpose of Loan and Credit Type") + labs(y="Count", x="Purpose of Loan") +
-  theme(plot.title = element_text(hjust=0.5))
-purpose_plot 
-
-
-# Creates a new dataframe that will allow us to reorder based on count of job column
-job_df <- credit_df %>%
-  group_by(job) %>%
-  mutate(job_occur=n())
-
-# Creates a plot based on the job dataframe, orders by descending values for count
-job_plot <- ggplot(job_df, aes(x=reorder(job, -job_occur), fill=class)) + 
-  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(name="Credit Type", values = class_palette) +
-  ggtitle("Count of Customers by Occupation and Credit Type") + labs(y="Count", x="Occupation of Applicant") +
-  theme(plot.title = element_text(hjust=0.5))
-job_plot 
-
-
-# Creates a new dataframe that will allow us to reorder based on count of credit_history column
-history_df <- credit_df %>%
-  group_by(credit_history) %>%
-  mutate(history_occur=n())
-
-# Creates a plot based on the history dataframe, orders by descending values for count
-history_plot <- ggplot(history_df, aes(x=reorder(credit_history, -history_occur), fill=class)) + 
-  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(name="Credit Type", values = class_palette) +
-  ggtitle("Count of Customers by Credit History and Credit Type") + labs(y="Count", x="Credit History") +
-  theme(plot.title = element_text(hjust=0.5))
-history_plot 
-
-
-# Creates a new dataframe that will allow us to reorder based on count of savings_status column
-savings_df <- credit_df %>%
-  group_by(savings_status) %>%
-  mutate(savings_occur=n())
-
-# Creates a plot based on the savings dataframe, orders by descending values for count
-savings_plot <- ggplot(savings_df, aes(x=reorder(savings_status, -savings_occur), fill=class)) + 
-  geom_bar(position="dodge", stat="count") + theme(axis.text.x = element_text(angle = 90)) +
-  scale_fill_manual(name="Credit Type", values = class_palette) +
-  ggtitle("Count of Customers by Savings and Credit Type") + labs(y="Count", x="Savings") +
-  theme(plot.title = element_text(hjust=0.5))
-savings_plot 
-
-
-# Creates a density plot based on original credit dataframe to show proportion of class values by credit_amount
-# low alpha value allows us to view overlapping densities
-amount_plot <- ggplot(credit_df, aes(x=credit_amount, fill=class)) + geom_density(alpha=0.50) +
-  scale_fill_manual(name="Credit Type", values = class_palette) +
-  ggtitle("Density of Customers by Loan Amount and Credit Type") + labs(y="Proportion", x="Loan Amount") +
-  theme(plot.title = element_text(hjust=0.5))
-amount_plot
-
-
-# Creates a density plot based on original credit dataframe to show proportion of class values by age
-# low alpha value allows us to view overlapping densities
-age_plot <- ggplot(credit_df, aes(x=age, fill=class)) + geom_density(alpha=0.50) +
-  scale_fill_manual(name="Credit Type", values = class_palette) +
-  ggtitle("Density of Customers by Age and Credit Type") + labs(y="Proportion", x="Age") +
-  theme(plot.title = element_text(hjust=0.5))
-age_plot
-
-
 # We create a new dataframe that oversamples from the 'bad' records for the class target variable
 credit_df_balanced <- ovun.sample(class~., data=credit_df, method="over", seed=42)$data
 table(credit_df_balanced$class) #table checks the number of each class value in balanced dataframe
@@ -185,46 +110,6 @@ property_plot
 #Creates a bar plot based on personal staus (sex and martial status) 
 personvcredit_plot = ggplot(data = credit_df, aes(x= personal_status, colour = class)) + geom_bar(position = "dodge", stat = "count")
 personvcredit_plot
-
-
-# Creates a function for an asymmetrical violin plot integrating a dichotomous variable.
-GeomSplitViolin <- ggproto("GeomSplitViolin", GeomViolin, 
-                           draw_group = function(self, data, ..., draw_quantiles = NULL) {
-                             data <- transform(data, xminv = x - violinwidth * (x - xmin), xmaxv = x + violinwidth * (xmax - x))
-                             grp <- data[1, "group"]
-                             newdata <- plyr::arrange(transform(data, x = if (grp %% 2 == 1) xminv else xmaxv), if (grp %% 2 == 1) y else -y)
-                             newdata <- rbind(newdata[1, ], newdata, newdata[nrow(newdata), ], newdata[1, ])
-                             newdata[c(1, nrow(newdata) - 1, nrow(newdata)), "x"] <- round(newdata[1, "x"])
-                             
-                             if (length(draw_quantiles) > 0 & !scales::zero_range(range(data$y))) {
-                               stopifnot(all(draw_quantiles >= 0), all(draw_quantiles <=
-                                                                         1))
-                               quantiles <- ggplot2:::create_quantile_segment_frame(data, draw_quantiles)
-                               aesthetics <- data[rep(1, nrow(quantiles)), setdiff(names(data), c("x", "y")), drop = FALSE]
-                               aesthetics$alpha <- rep(1, nrow(quantiles))
-                               both <- cbind(quantiles, aesthetics)
-                               quantile_grob <- GeomPath$draw_panel(both, ...)
-                               ggplot2:::ggname("geom_split_violin", grid::grobTree(GeomPolygon$draw_panel(newdata, ...), quantile_grob))
-                             }
-                             else {
-                               ggplot2:::ggname("geom_split_violin", GeomPolygon$draw_panel(newdata, ...))
-                             }
-                           })
-
-geom_split_violin <- function(mapping = NULL, data = NULL, stat = "ydensity", position = "identity", ..., 
-                              draw_quantiles = NULL, trim = TRUE, scale = "area", na.rm = FALSE, 
-                              show.legend = NA, inherit.aes = TRUE) {
-  layer(data = data, mapping = mapping, stat = stat, geom = GeomSplitViolin, 
-        position = position, show.legend = show.legend, inherit.aes = inherit.aes, 
-        params = list(trim = trim, scale = scale, draw_quantiles = draw_quantiles, na.rm = na.rm, ...))
-}
-
-# Create a split violin plot for savings and credit, distinguishing good from bad classes of credit.
-credit_by_savings_plot <- ggplot(savings_df_balanced, aes(x=reorder(savings_status, -savings_occur), 
-                                                          y=credit_amount, fill=class)) +
-  geom_split_violin() + theme_minimal() + 
-  labs(x="Savings Status", y="Credit",title="Credit Density by Savings and Class")
-credit_by_savings_plot
 
 
 #Density Residence since_SM
